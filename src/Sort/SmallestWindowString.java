0c22497431fff452f316cdebd1bb71d37847c2d6
DELL <vuhson30799@gmail.com>
2018-11-22 00:37:00 +0700

    finish SmallestWindowString

--- src/Sort/SmallestWindowString.java
+++ src/Sort/SmallestWindowString.java
@@ -1,61 +1,58 @@
 package Sort;
 
-import java.util.LinkedHashMap;
+
+import javax.print.DocFlavor;
 import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
 
 public class SmallestWindowString {
     public static void main(String[] args) {
         String string = "welcome to w3resource";
         String subString = "tower";
-        Character[] indexOfSubString = new Character[string.length()];
-        char[] charsOfString = string.toCharArray();;
+
+        LinkedList<Integer> indexOfSubString = new LinkedList<>();
+
+        char[] charsOfString = string.toCharArray();
 
         char[] charsOfSubString = subString.toCharArray();
+
+        Integer[] indexOfCharacter = new Integer[charsOfString.length];
+
+
         for (int i = 0; i < charsOfSubString.length; i++) {
             for (int j = 0; j < charsOfString.length; j++) {
                 if (charsOfSubString[i] == charsOfString[j]){
-                     indexOfSubString[j] = charsOfString[j];
+                     indexOfCharacter[j] = j;
                 }
             }
         }
 
-        // get the first index
-        int length = subString.length(), count = 0,index = -1,indexOfLast = 0;
+
+        for (int i = 0; i < indexOfCharacter.length; i++) {
+            if (indexOfCharacter[i] != null){
+                indexOfSubString.add(indexOfCharacter[i]);
+            }
+        }
+
+
+        int length = subString.length(),index = -1,indexOfLast = 0;
         boolean check = false;
-        for (int i = 0; i < indexOfSubString.length; i++) {
-            if (indexOfSubString[i] != null){
-                index = i;
-                break;
-            }
-        }
 
-        //get the last index
-        for (int i = indexOfSubString.length - 1; i >= 0; i--) {
-            if (indexOfSubString[i] != null){
-                indexOfLast = i;
-                break;
-            }
-        }
-        char[] result;
+
         while (length <= string.length()){
-            result = new char[length];
-            count = 0;
             index++;
-            for (int i = index; i < indexOfSubString.length; i++) {
-                if (indexOfSubString[i] != null){
-                    result[count] = indexOfSubString[i];
-                    count ++;
-                }
-                if (count == length){
-                    break;
-                }
+            indexOfLast = index + charsOfSubString.length - 1;
+            if (index > indexOfSubString.size() - charsOfSubString.length) {
+                length++;
+                index = -1;
+                continue;
+            }
+            if (indexOfSubString.get(indexOfLast) - indexOfSubString.get(index) + 1> length){
+                continue;
             }
             for (int i = 0; i < subString.length(); i++) {
                 check = false;
-                for (int j = 0; j < result.length; j++) {
-                    if (charsOfSubString[i] == result[j]){
+                for (int j = index; j <= indexOfLast; j++) {
+                    if (charsOfSubString[i] == charsOfString[indexOfSubString.get(j)]){
                         check = true;
                         break;
                     }
@@ -66,22 +63,12 @@ public class SmallestWindowString {
                 }
             }
             if (check) {
-                for (int i = index; i < indexOfSubString.length; i++) {
-                    if (indexOfSubString[i] != null){
-                        count--;
-                    }
-                    if (count == 0) {
-                        indexOfLast = i;
-                        break;
-                    }
-                }
                 break;
             }
-            if (result[result.length - 1] == charsOfString[indexOfLast]) {
-                length++;
-            }
         }
-        for (int i = indexOfLast - length; i <= indexOfLast ; i++) {
+
+
+        for (int i = indexOfSubString.get(index); i <= indexOfSubString.get(indexOfLast) ; i++) {
             System.out.print(charsOfString[i]);
         }
     }
