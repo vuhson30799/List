c2b27a24cb33414813a9547f7cd71d6dcd11bc67
DELL <vuhson30799@gmail.com>
2018-11-28 09:11:36 +0700

    crawl some String

--- src/RegularExpression/CrawlData.java
+++ src/RegularExpression/CrawlData.java
@@ -1,5 +1,26 @@
 package RegularExpression;
 
-public class CrawlData {
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class CrawlData {
+    public static String REGEX_SONG = "name_song\".*?\"(.*?)\">(.*?) - (.*?)</a>";
+
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("<a class=\"name_song\" href=\"nct.com/concobebe\">Con co be be - Dung</a>");
+        arr.add("<a class=\"name_song\" href=\"nct.com/backimthang\">Bac kim thang - Dung</a>");
+        arr.add("<a class=\"name_song\" href=\"nct.com/hongkong1\">Hongkong1 - Son</a>");
+        arr.add("<a class=\"name_song\" href=\"nct.com/lactroi\">Lac Troi - Loc</a>");
+        arr.add("<a class=\"name_song\" href=\"nct.com/yeu\">Yeu - Vu</a>");
+        for (String x :
+                arr) {
+            Pattern p = Pattern.compile(REGEX_SONG);
+            Matcher m = p.matcher(x);
+            while (m.find()){
+                System.out.println(m.group(1) + " - " + m.group(2) + " - " + m.group(3));
+            }
+        }
+    }
 }
